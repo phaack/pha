3fa97bd4376e0af17927275fb595cac1ae8b87e3
Philipp Haack <PhilippHaackCode@gmail.com>
2025-04-14 22:30:00 +0200

    fix: refactor move logic to commin

--- crates/pha-client/src/player/movement.rs
+++ crates/pha-client/src/player/movement.rs
@@ -1,10 +1,7 @@
 use bevy::prelude::*;
-use pha_common::player::move_player;
 
 pub struct ClientMovementPlugin;
 
 impl Plugin for ClientMovementPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(FixedUpdate, move_player);
-    }
+    fn build(&self, app: &mut App) {}
 }
--- crates/pha-server/src/player/movement.rs
+++ crates/pha-server/src/player/movement.rs
@@ -1,10 +1,7 @@
 use bevy::prelude::*;
-use pha_common::player::move_player;
 
 pub(crate) struct MovementPlugin;
 
 impl Plugin for MovementPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(FixedUpdate, move_player);
-    }
+    fn build(&self, app: &mut App) {}
 }
